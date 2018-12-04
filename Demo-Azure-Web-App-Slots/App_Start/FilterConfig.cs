9dae6cf882d7c4c20d84a42124cf8321ba80b0ea
Adam Paternostro <adampaternostro@Adams-MacBook-Pro.local>
2018-12-04 17:40:11 -0500

    Done

---
+++ Demo-Azure-Web-App-Slots/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_Azure_Web_App_Slots
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Demo-Azure-Web-App-Slots/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo_Azure_Web_App_Slots.MvcApplication" Language="C#" %>
